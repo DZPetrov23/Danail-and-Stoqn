cf01cd669f4e3abbc42a9f5f9e66264b9c80017a
Данаил Златомиров Петров <DZPetrov23@codingburgas.bg>
2026-02-03 10:25:46 +0200

    Add library file

--- Danail-and-Stoqn/Project1/library.h
+++ Danail-and-Stoqn/Project1/library.h
@@ -1,1 +1,32 @@
 #pragma once
+#ifndef LIBRARY_H
+#define LIBRARY_H
+#include <string>
+using namespace std;
+
+struct Book {
+    string title;
+    string author;
+    int year;
+    double price;
+};
+
+struct Library {
+    string name;
+    Book* books;    
+    int count;
+};
+
+Book createBook(const string& title,
+    const string& author,
+    int year,
+    double price);
+
+void printBook(const Book& book);
+
+void printLibrary(const Library& library);
+
+double averageBookPrice(const Library& library);
+
+Book oldestBookInLibrary(const Library& library);
+#endif
