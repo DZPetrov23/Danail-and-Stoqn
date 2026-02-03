77d1da8ad2cc321cd989636f4d45533fdf698d0a
Данаил Златомиров Петров <DZPetrov23@codingburgas.bg>
2026-02-03 10:41:27 +0200

    Add main file

--- Danail-and-Stoqn/Danail-and-Stoqn/Danail-and-Stoqn.cpp
+++ Danail-and-Stoqn/Danail-and-Stoqn/Danail-and-Stoqn.cpp
@@ -1,20 +1,26 @@
-// Danail-and-Stoqn.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "../Project1/library.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+void testLibrary() {
+
+	Book b1 = createBook("Book One", "Author A", 2000, 20.50);
+	Book b2 = createBook("Book Two", "Author B", 1995, 15.75);
+
+	Library lib;
+	lib.name = "Test Library";
+	lib.count = 2;
+	lib.books = new Book[lib.count];
+	lib.books[0] = b1;
+	lib.books[1] = b2;
+
+	printLibrary(lib);
+	cout << "Average: " << averageBookPrice(lib) << endl;
+	Book oldest = oldestBookInLibrary(lib);
+	cout << "Oldest: ";
+	printBook(oldest);
+	delete[] lib.books;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main() {
+	testLibrary();
+}
